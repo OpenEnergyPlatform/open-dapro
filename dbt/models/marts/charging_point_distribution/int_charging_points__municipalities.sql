11ba3983c8699eb8f3c95fb6cbfd911dcf589be5
Grazvy <grazbvydas.kuras@gmail.com>
2023-07-08 17:39:00 +0200

    logical steps for charging points per district/municipality

---
+++ dbt/models/marts/charging_point_distribution/int_charging_points__municipalities.sql
@@ -0,0 +1,21 @@
+with municipalities_area as (
+    select * from {{ ref('stg_municipalities__area') }}
+),
+
+charging_points as (
+    select
+        amount_charging_points,
+        geo_point
+    from {{ ref('stg_charging_points') }}
+),
+
+final as (
+    select
+        municipality_id,
+        sum(amount_charging_points) as amount_charging_points
+    from municipalities_area left join charging_points
+        on st_contains(geometry_array, geo_point)
+    group by municipality_id
+)
+
+select * from final
