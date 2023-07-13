8f98d8625af2af3cef404982df4377d7f171231a
Florian Kotthoff <florian.kotthoff@posteo.de>
2023-07-13 17:12:48 +0200

    Delete unnecesarry try except statements

--- energy_dagster/assets/load_data/charging_points.py
+++ energy_dagster/assets/load_data/charging_points.py
@@ -6,11 +6,8 @@ import urllib.request as request
 
 
 @asset(io_manager_key="db_io", key_prefix="raw", group_name="raw_data")
-def charging_points(context) -> pd.DataFrame:
-    try:
-        constants = utils.get_constants("ladesaeulenregister")
-        raw_data = request.urlopen(constants["url"]).read()
-        df = pd.read_excel(raw_data, skiprows=10)
-        return df
-    except Exception as e:
-        context.log.info(str(e))
+def charging_points() -> pd.DataFrame:
+    constants = utils.get_constants("ladesaeulenregister")
+    raw_data = request.urlopen(constants["url"]).read()
+    df = pd.read_excel(raw_data, skiprows=10)
+    return df
--- energy_dagster/assets/load_data/mastr.py
+++ energy_dagster/assets/load_data/mastr.py
@@ -6,37 +6,26 @@ from energy_dagster.utils import utils
 
 
 @asset(key_prefix="raw_mastr", group_name="mastr")
-def wind_extended(context) -> None:
-    try:
-        engine = utils.get_engine()
-        db = Mastr(engine=engine)
-        db.download(date=os.environ["MASTR_DOWNLOAD_DATE"], data=["wind"])
-
-    except Exception as e:
-        context.log.info(str(e))
+def wind_extended() -> None:
+    engine = utils.get_engine()
+    db = Mastr(engine=engine)
+    db.download(date=os.environ["MASTR_DOWNLOAD_DATE"], data=["wind"])
 
 
 @asset(key_prefix="raw_mastr", group_name="mastr", non_argument_deps={"wind_extended"})
-def biomass_extended(context) -> None:
-    try:
-        engine = utils.get_engine()
-        db = Mastr(engine=engine)
-        db.download(date=os.environ["MASTR_DOWNLOAD_DATE"], data=["biomass"])
-
-    except Exception as e:
-        context.log.info(str(e))
+def biomass_extended() -> None:
+    engine = utils.get_engine()
+    db = Mastr(engine=engine)
+    db.download(date=os.environ["MASTR_DOWNLOAD_DATE"], data=["biomass"])
 
 
 @asset(
     key_prefix="raw_mastr", group_name="mastr", non_argument_deps={"biomass_extended"}
 )
-def storage_extended(context) -> None:
-    try:
-        engine = utils.get_engine()
-        db = Mastr(engine=engine)
-        db.download(date=os.environ["MASTR_DOWNLOAD_DATE"], data=["storage"])
-    except Exception as e:
-        context.log.info(str(e))
+def storage_extended() -> None:
+    engine = utils.get_engine()
+    db = Mastr(engine=engine)
+    db.download(date=os.environ["MASTR_DOWNLOAD_DATE"], data=["storage"])
 
 
 @asset(
@@ -49,22 +38,14 @@ def storage_units() -> None:
 @asset(
     key_prefix="raw_mastr", group_name="mastr", non_argument_deps={"storage_extended"}
 )
-def solar_extended(context) -> None:
-    try:
-        engine = utils.get_engine()
-        db = Mastr(engine=engine)
-        db.download(date=os.environ["MASTR_DOWNLOAD_DATE"], data=["solar"])
-
-    except Exception as e:
-        context.log.info(str(e))
+def solar_extended() -> None:
+    engine = utils.get_engine()
+    db = Mastr(engine=engine)
+    db.download(date=os.environ["MASTR_DOWNLOAD_DATE"], data=["solar"])
 
 
 @asset(key_prefix="raw_mastr", group_name="mastr", non_argument_deps={"solar_extended"})
-def market_actors(context) -> None:
-    try:
-        engine = utils.get_engine()
-        db = Mastr(engine=engine)
-        db.download(date=os.environ["MASTR_DOWNLOAD_DATE"], data=["market"])
-
-    except Exception as e:
-        context.log.info(str(e))
+def market_actors() -> None:
+    engine = utils.get_engine()
+    db = Mastr(engine=engine)
+    db.download(date=os.environ["MASTR_DOWNLOAD_DATE"], data=["market"])
