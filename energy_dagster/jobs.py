0d04ca02815ceacb4b2d945eec6c36a56600142d
Florian Kotthoff <florian.kotthoff@posteo.de>
2023-07-18 15:50:28 +0200

    Reduce line length because of linting

--- energy_dagster/jobs.py
+++ energy_dagster/jobs.py
@@ -1,5 +1,4 @@
-from dagster import AssetSelection, define_asset_job, ScheduleDefinition
-
+from dagster import AssetSelection, ScheduleDefinition, define_asset_job
 
 mastr_asset_job = define_asset_job(
     "download_mastr", AssetSelection.groups("mastr", "staging", "marts")
@@ -10,7 +9,8 @@ raw_data_asset_job = define_asset_job(
 # cron_schedule="0 12 * * 0" means that the job will run every Sunday at 12:00
 # To get cron_schedule, use https://crontab.guru/
 mastr_schedule = ScheduleDefinition(job=mastr_asset_job, cron_schedule="0 12 * * 0")
-# cron_schedule="0 1 11 * *" means that the job will run at the 11th of every month at 1:00
+# cron_schedule="0 1 11 * *" means that the job will run at the 11th
+# of every month at 1:00
 raw_data_schedule = ScheduleDefinition(
     job=raw_data_asset_job, cron_schedule="0 11 11 * *"
 )
