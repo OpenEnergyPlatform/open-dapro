d91ca835299d1659233ffcdd059c85953a73df09
Florian Kotthoff <florian.kotthoff@posteo.de>
2023-07-18 17:59:35 +0200

    Add docstring to function `charging_points`

--- energy_dagster/assets/load_data/charging_points.py
+++ energy_dagster/assets/load_data/charging_points.py
@@ -1,12 +1,22 @@
-from dagster import asset
+import urllib.request as request
 
 import pandas as pd
+from dagster import asset
+
 from energy_dagster.utils import utils
-import urllib.request as request
 
 
 @asset(io_manager_key="db_io", key_prefix="raw", group_name="raw_data")
 def charging_points() -> pd.DataFrame:
+    """Download charging points data from Ladesaeulenregister offered by
+    the german 'bundesnetzagentur'. The link to the dataset can be obtained
+    from the `constants.yaml` file in the energy_dagster folder.
+
+    Returns
+    -------
+    pd.DataFrame
+        Charging points in its raw data format.
+    """
     constants = utils.get_constants("ladesaeulenregister")
     raw_data = request.urlopen(constants["url"]).read()
     df = pd.read_excel(raw_data, skiprows=10)
