07e11cae3957180bf7efd2c893ef70060da47ee6
Florian Kotthoff <florian.kotthoff@posteo.de>
2023-07-18 09:36:21 +0200

    Delete unnecessary try expcept blog

--- energy_dagster/assets/load_data/zensus.py
+++ energy_dagster/assets/load_data/zensus.py
@@ -8,15 +8,10 @@ import zipfile
 
 
 @asset(io_manager_key="db_io", key_prefix="raw", group_name="raw_data")
-def zensus(context) -> pd.DataFrame:
-    try:
-        constants = utils.get_constants("zensus")
-        zipped_data = request.urlopen(constants["url"]).read()
-        with zipfile.ZipFile(io.BytesIO(zipped_data)) as zip_ref:
-            with zip_ref.open("Zensus11_Datensatz_Bevoelkerung.csv") as file:
-                df = pd.read_csv(
-                    file, encoding="latin-1", delimiter=";", low_memory=False
-                )
-        return df
-    except Exception as e:
-        context.log.info(str(e))
+def zensus() -> pd.DataFrame:
+    constants = utils.get_constants("zensus")
+    zipped_data = request.urlopen(constants["url"]).read()
+    with zipfile.ZipFile(io.BytesIO(zipped_data)) as zip_ref:
+        with zip_ref.open("Zensus11_Datensatz_Bevoelkerung.csv") as file:
+            df = pd.read_csv(file, encoding="latin-1", delimiter=";", low_memory=False)
+    return df
