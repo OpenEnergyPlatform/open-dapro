2965ac292e1f4f176bd3b9acc1f58db0e6d43e6d
Grazvy <kuras@fortiss.org>
2023-08-21 18:44:41 +0200

    added district_id

---
+++ dbt/models/staging/zensus/stg_zensus.sql
@@ -0,0 +1,14 @@
+with source as (
+    select * from {{ ref('base_zensus') }}
+)
+
+select
+    community_name,
+    number_inhabitants,
+    federal_state_in_ags
+    || nuts2_in_ags
+    || district_in_ags
+    || municipality_in_ags as municipality_id,
+    federal_state_in_ags || nuts2_in_ags || district_in_ags as district_id
+from source
+where regional_level = 'Gemeinde'
