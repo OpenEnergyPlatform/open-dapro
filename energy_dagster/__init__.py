c2c1afe0bb95e3cd24d03a1a17f940c292f11161
Florian Kotthoff <florian.kotthoff@posteo.de>
2023-09-07 16:45:07 +0200

    Add a dbt postgis io mananger

--- energy_dagster/__init__.py
+++ energy_dagster/__init__.py
@@ -1,11 +1,11 @@
+import os
+
 from dagster import Definitions, FilesystemIOManager, load_assets_from_modules
 from dagster_dbt import DbtCliClientResource
 
-from energy_dagster.io import db_io_manager, postgis_io_manager
 from energy_dagster import assets
 from energy_dagster.assets import DBT_PROFILES_PATH, DBT_PROJECT_PATH
-import os
-
+from energy_dagster.io import db_io_manager, postgis_io_manager
 from energy_dagster.jobs import mastr_schedule, raw_data_schedule
 
 BASE_DIR = os.path.join(os.path.expanduser("~"), ".dagster", "dagster_energy_files")
@@ -20,6 +20,16 @@ resources = {
             "pwd": {"env": "pwd"},
             "port": {"env": "port"},
             "schema": {"env": "schema"},
+        }
+    ),
+    "dbt_marts_to_geopandas": postgis_io_manager.postgis_geopandas_io_manager.configured(
+        {
+            "server": {"env": "server"},
+            "db": {"env": "db"},
+            "uid": {"env": "uid"},
+            "pwd": {"env": "pwd"},
+            "port": {"env": "port"},
+            "schema": "data_marts",
         }
     ),
     "postgis_io": postgis_io_manager.postgis_geopandas_io_manager.configured(
