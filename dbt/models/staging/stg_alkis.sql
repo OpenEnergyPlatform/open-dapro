e36472f79877c55a866e8aa14680d83030fe3432
Florian Kotthoff <florian.kotthoff@posteo.de>
2023-09-07 16:43:41 +0200

    Replace german characters: ä->ae etc.

--- dbt/models/staging/stg_alkis.sql
+++ dbt/models/staging/stg_alkis.sql
@@ -10,7 +10,7 @@ WITH source_alkis AS (
     SELECT * FROM {{ source('raw', 'load_alkis') }}
 ),
 
-final AS (
+selected_rows AS (
     SELECT
         gml_id AS alkis_id,
         aktualit AS updated_at,
@@ -26,6 +26,17 @@ final AS (
         'Landwirtschaft',
         'Wohnbaufläche'
     )
+),
+
+final AS (
+    SELECT
+        alkis_id,
+        updated_at,
+        REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(usage_type, 'ä', 'ae'), 'ö', 'oe'), 'ü', 'ue'), 'ß', 'ss'), 'Ä', 'Ae'), 'Ü', 'Ue'), 'Ö', 'Oe') AS usage_type,
+        REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(usage_type_specification, 'ä', 'ae'), 'ö', 'oe'), 'ü', 'ue'), 'ß', 'ss'), 'Ä', 'Ae'), 'Ü', 'Ue'), 'Ö', 'Oe') AS usage_type_specification,
+        geometry
+    FROM
+        selected_rows
 )
 
 SELECT * FROM final
