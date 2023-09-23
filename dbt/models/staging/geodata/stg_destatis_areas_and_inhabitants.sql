6f086153ab3ff14f014710e3e8c5d4999ff7b6a4
Grazvy <kuras@fortiss.org>
2023-09-23 17:54:11 +0200

    fixed bug with wrong column names

--- dbt/models/staging/geodata/stg_destatis_areas_and_inhabitants.sql
+++ dbt/models/staging/geodata/stg_destatis_areas_and_inhabitants.sql
@@ -4,31 +4,31 @@ with source as (
 
 municipalities_filtered as (
     select
-        LPAD(source."01"::text, 2, '0') as federal_state,
         source."Unnamed: 3"::text as nuts,
+        source."Unnamed: 7" as municipality,
+        source."Unnamed: 10" as number_inhabitants,
+        LPAD(source."Unnamed: 2"::text, 2, '0') as federal_state,
         LPAD(source."Unnamed: 4"::text, 2, '0') as d_id,
-        LPAD(source."Unnamed: 6"::text, 3, '0') as m_id,
-        source."Schleswig-Holstein" as municipality,
-        source."Unnamed: 10" as number_inhabitants
+        LPAD(source."Unnamed: 6"::text, 3, '0') as m_id
     from source
     where source."Unnamed: 6" is not null and source."Unnamed: 4" is not null
 ),
 
 municipalities as (
-  select
-      municipality,
-      federal_state || nuts || d_id || m_id as municipality_id,
-      federal_state || nuts || d_id as district_id,
-      number_inhabitants
-  from municipalities_filtered
+    select
+        municipality,
+        number_inhabitants,
+        federal_state || nuts || d_id || m_id as municipality_id,
+        federal_state || nuts || d_id as district_id
+    from municipalities_filtered
 ),
 
 districts_filtered as (
     select
-        LPAD(source."01"::text, 2, '0') as federal_state,
         source."Unnamed: 3"::text as nuts,
-        LPAD(source."Unnamed: 4"::text, 2, '0') as d_id,
-        source."Schleswig-Holstein" as district
+        source."Unnamed: 7" as district,
+        LPAD(source."Unnamed: 2"::text, 2, '0') as federal_state,
+        LPAD(source."Unnamed: 4"::text, 2, '0') as d_id
     from source
     where source."Unnamed: 4" is not null and source."Unnamed: 5" is null
 ),
