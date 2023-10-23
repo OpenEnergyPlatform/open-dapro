5f62ebb782ca7f244521d5e81fc19e92d304abd3
Florian Kotthoff <kotthoff@fortiss.org>
2023-10-23 08:34:28 +0200

    Replace non utf characters in files

--- energy_dagster/assets/analysis/districts_to_datasette.py
+++ energy_dagster/assets/analysis/districts_to_datasette.py
@@ -1,7 +1,6 @@
 import os
 
 from dagster import AssetIn, asset
-from sqlalchemy import create_engine
 
 from energy_dagster.utils import utils
 
@@ -35,9 +34,124 @@ def buildings_to_datasette(context, marts_buildings) -> None:
     # marts_buildings.to_sql(
     #    con=create_engine("sqlite:///" + sqlite_path), name="buildings"
     # )
-    # marts_buildings.replace(
-    #    to_replace=["ä", "ö", "ü", "ß"], value=["ae", "oe", "ue", "ss"], inplace=True
-    # )
+    marts_buildings.replace(
+        to_replace=[
+            "ä",
+            "ö",
+            "ü",
+            "Ä",
+            "Ö",
+            "Ü",
+            "ß",
+            "ą",
+            "ć",
+            "ę",
+            "ł",
+            "ń",
+            "ó",
+            "ś",
+            "ź",
+            "ż",
+            "á",
+            "č",
+            "ď",
+            "é",
+            "ě",
+            "í",
+            "ň",
+            "ó",
+            "ř",
+            "š",
+            "ť",
+            "ú",
+            "ů",
+            "ý",
+            "ž",
+            "Á",
+            "Č",
+            "Ď",
+            "É",
+            "Ě",
+            "Í",
+            "Ň",
+            "Ó",
+            "Ř",
+            "Š",
+            "Ť",
+            "Ú",
+            "Ů",
+            "Ý",
+            "Ž",
+            "Ą",
+            "Ć",
+            "Ę",
+            "Ł",
+            "Ń",
+            "Ó",
+            "Ś",
+            "Ź",
+            "Ż",
+        ],
+        value=[
+            "ae",
+            "oe",
+            "ue",
+            "Ae",
+            "Oe",
+            "Ue",
+            "ss",
+            "a",
+            "c",
+            "e",
+            "l",
+            "n",
+            "o",
+            "s",
+            "z",
+            "z",
+            "a",
+            "c",
+            "d",
+            "e",
+            "e",
+            "i",
+            "n",
+            "o",
+            "r",
+            "s",
+            "t",
+            "u",
+            "u",
+            "y",
+            "z",
+            "A",
+            "C",
+            "D",
+            "E",
+            "E",
+            "I",
+            "N",
+            "O",
+            "R",
+            "S",
+            "T",
+            "U",
+            "U",
+            "Y",
+            "Z",
+            "A",
+            "C",
+            "E",
+            "L",
+            "N",
+            "O",
+            "S",
+            "Z",
+            "Z",
+        ],
+        inplace=True,
+        regex=True,
+    )
     # marts_buildings = marts_buildings.map(replace_german_chars)
 
     # print(marts_buildings.head())
@@ -46,3 +160,13 @@ def buildings_to_datasette(context, marts_buildings) -> None:
         filename=geojson_path, driver="GeoJSON"
     )
     context.log.info(len(marts_buildings))
+    command = f"geojson-to-sqlite {sqlite_path} buildings_json {geojson_path}"
+    # create_sqlite_db = create_shell_command_op(
+    #    command,
+    #    name="create_sqlite_db",
+    # )
+    # create_sqlite_db()
+    create_sqlite_stream = os.popen(command)
+    output = create_sqlite_stream.read()
+
+    print(output)
